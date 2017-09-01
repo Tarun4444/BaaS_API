de9ec0c6cc4183a7981d1af2e405e5e3d165416b
Tarun4444 <tarungulati2013@gmail.com>
2017-09-01 15:22:28 +0530

    1

--- app/src/currentuser.php
+++
@@ -1,9 +0,0 @@
-
-
-<?php
-	if(!isset($_SESSION["fn"]))
-		header("location:login.php");
-?>
-
-<span style="float:right"><?php echo $_SESSION["fn"];?>, [ <a href="logout.php">logout</a> ] 
-</span>
