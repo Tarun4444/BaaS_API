e2e761a07b54118239844ac6836a679018dd5249
Tarun4444 <tarungulati2013@gmail.com>
2017-09-02 17:04:10 +0530

    1

--- app/src/footer.php
+++ app/src/footer.php
@@ -1,8 +1,8 @@
 <div class="cleared"></div><div class="art-Footer">
                     <div class="art-Footer-inner">
                         <div class="art-Footer-text">
-                            <p><a href="contact.php">Credits</a> | <a href="terms.php">Site rules</a>
-                                | <a href="prvs.php">FAQ</a> | <a href="isuser.php">Home page</a><br />
+                            <p><a href="contact.php">Credits</a> 
+                     			| <a href="prvs.php">FAQ</a> | <a href="isuser.php">Home page</a><br />
                                 </p>
                         </div>
                     </div>
