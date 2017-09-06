c331422311603505aab5064a3312e7b64d2e57b8
Tarun4444 <tarungulati2013@gmail.com>
2017-09-06 15:22:41 +0530

    2

--- app/src/register.php
+++ app/src/register.php
@@ -7,6 +7,11 @@
 		form1.f_name.value == "" ||
 		form1.pwd.value == "" ||
 		form1.e_mail.value == ""||
+		form1.gender.value == ""||
+		form1.dob.value == ""||
+		form1.add.value == ""||
+		form1.sta.value == "" ||
+		form1.cou.value == "" )
 		{
 		
 			if (form1.u_name.value == "")
@@ -43,7 +48,13 @@
 				form1.pwd.focus();
 				
 			}
-			
+			else
+			{
+				document.getElementById("c").innerHTML = "";
+				//alert ("Please,Please Enter The Password");
+				form1.pwd.focus();
+
+			}
 			
 			 if (form1.e_mail.value == "")
 			{
@@ -57,6 +68,18 @@
 				document.getElementById("d").innerHTML = "";
 				//alert ("Please,Please Enter The E-mail Address");
 				form1.e_mail.focus();
+			}
+			 if (form1.gender.value == "")
+			{
+				document.getElementById("spuid").innerHTML = "Please, Enter the gender.";
+				//alert("Please,Please Enter The Gender");
+				form1.gender.focus();
+				
+			}else
+			{
+				document.getElementById("spuid").innerHTML = "";
+				//alert("Please,Please Enter The Gender");
+				form1.gender.focus();
 			}
 			 if (form1.dob.value == "")
 			{
@@ -72,22 +95,70 @@
 				form1.dob.focus();
 
 			}
-			 
-		}
-	}
+			 if (form1.add.value == "")
+			{
+				document.getElementById("f").innerHTML = "Please, Enter address.";
+				//alert ("Please,Please Enter The Address");
+				form1.add.focus();
+				
+			}
+			else
+			{
+				document.getElementById("f").innerHTML = "";
+				//alert ("Please,Please Enter The Address");
+				form1.add.focus();
+			}
+			 if (form1.sta.value == "")
+			{
+				document.getElementById("g").innerHTML = "Please, Enter state.";
+				//alert ("Please,Please Enter The State");
+				form1.sta.focus();
+				
+			}
+			else
+			{
+				document.getElementById("g").innerHTML = "";
+				//alert ("Please,Please Enter The State");
+				form1.sta.focus();
 
-<html>
+			}
+			 if (form1.cou.value == "")
+			{
+				document.getElementById("h").innerHTML = "Please, Enter country.";
+				//alert ("Please,Please Enter The Country");
+				form1.cou.focus();
+				
+			}
+			else
+			{
+				document.getElementById("h").innerHTML = "";
+				//alert ("Please,Please Enter The Country");
+				form1.cou.focus();
+			}
+			return false;
+		}
+		else
+			return true;
+	}
+</script>
+
+
+
+
 <h1>Register User</h1>
 <form action="registerH.php" method="post" onsubmit="return check(this)" enctype="multipart/form-data" name = "form1">
-	<table>
-		<tr><td>Username</td><td>:</td><td><input type="text" name="u_name" ><span id='a' style="color: red;"></span></td></tr>
-		<tr><td>Fullname</td><td>:</td><td><input type="text" name="f_name"><span id='b' style="color: red;"></span></td></tr>
-		<tr><td>Password</td><td>:</td><td><input type="password" name="pwd"><span id='c' style="color: red;"></span></td></tr>
-		<tr><td>E_Mail</td><td>:</td><td><input type="text" name="e_mail"><span id='d' style="color: red;"></span></td></tr>
-		<tr><td><input type="submit" value="Submit"></td><td></td><td><input type="reset" value="Reset"></td></tr>
-	</table>
-</form>
-</html>
+<table>
+<tr><td>Username</td><td>:</td><td><input type="text" name="u_name" ><span id='a' style="color: red;"></span></td></tr>
+<tr><td>Fullname</td><td>:</td><td><input type="text" name="f_name"><span id='b' style="color: red;"></span></td></tr>
+<tr><td>Password</td><td>:</td><td><input type="password" name="pwd"><span id='c' style="color: red;"></span></td></tr>
+<tr><td>E_Mail</td><td>:</td><td><input type="text" name="e_mail"><span id='d' style="color: red;"></span></td></tr>
+<tr><td>Gender</td><td>:</td><td><input type="radio" name="gender" value="1" checked="checked">male <input type="radio" name="gender" value="2">female<span id='spuid' style="color: red;"></span></td></tr>
+<tr><td>Date Of Birth</td><td>:</td><td><input type="text" name="dob" ><span id='e' style="color: red;"></span></td></tr>
+<tr><td>Image</td><td>:</td><td><input type="file" name="ima"></td></tr>
+<tr><td>Address</td><td>:</td><td><textarea rows="3" cols="15" name="add"></textarea><span id='f' style="color: red;"></span></td></tr>
+<tr><td>State</td><td>:</td><td><input type="text" name="sta"><span id='g' style="color: red;"></span></td></tr>
+<tr><td>Country</td><td>:</td><td><input type="text" name="cou"><span id='h' style="color: red;"></span></td></tr>
 
+<tr><td><input type="submit" value="Submit"></td><td></td><td><input type="reset" value="Reset"></td></tr></table></form>
 
 <?php require("footer.php"); ?>
