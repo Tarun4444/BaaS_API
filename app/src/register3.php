3bdc54f7428601e830555b9df8f079df54cf885e
Tarun4444 <tarungulati2013@gmail.com>
2017-09-06 17:20:52 +0530

    2

--- app/src/register3.php
+++ app/src/register3.php
@@ -1,4 +1,4 @@
 <?php 
  require("header.php"); ?>
- <h1>Your Profile Is Successfully Registered </h1>
+ <h1>Your Profile Is Successfully Registered . click <a href = "index.php"> <h4> here <h4> </a> to login </h1>
  <?php require("footer.php")?>
