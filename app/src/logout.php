f8d0b499a0bc18ac44f95721a41015866732bd0b
Tarun4444 <tarungulati2013@gmail.com>
2017-09-08 11:34:14 +0530

    2

--- app/src/logout.php
+++ app/src/logout.php
@@ -4,18 +4,18 @@
 <script>
 
 function logout_query(){
-request.open('GET'," https://auth.bewitch58.hasura-app.io/logout ", true);
-request.withCredentials=true;
-request.setRequestHeader('Authorization','Bearer');
 var request= new XMLHttpRequest();
 request.onreadystatechange= function(){
-   if(request.status===200){     
+   if(request.readyState===XMLHttpRequest.DONE && request.status===200){     
         window.location.href = "https://myapp.bewitch58.hasura-app.io/index.php";	
     }
     else{
 	console.log("error");	
 	}          
   }
+request.open('GET'," https://auth.bewitch58.hasura-app.io/logout ", true);
+request.withCredentials=true;
+request.setRequestHeader('Authorization','Bearer');
 console.log('Bearer');
 }
 
