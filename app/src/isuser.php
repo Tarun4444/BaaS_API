3440eb63d604698db6c42c9cdb5f0b0663b61514
Tarun4444 <tarungulati2013@gmail.com>
2017-09-10 21:13:04 +0530

    2

--- app/src/isuser.php
+++ app/src/isuser.php
@@ -7,11 +7,12 @@ require("header.php");
                          
     window.onload=function() {
    	var auth_toke = "; "+document.cookie ;
+	var poop=auth_toke.search("authio");
 	auth_toke = auth_toke.split("; authio=");
 	if(auth_toke.length==2)
 	 auth_toke=auth_toke.pop().split(";").shift(); 
         console.log(auth_toke);
-        if(auth_toke==""){
+        if(poop==-1){
        	     window.location.href = "https://myapp.bewitch58.hasura-app.io/index.php";
         }
         else{
