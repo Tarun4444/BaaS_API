557e6db792da2cb9ebb62c9c19c88259b795ed8c
Tarun4444 <tarungulati2013@gmail.com>
2017-09-11 01:19:18 +0530

    2

--- app/src/uhome.php
+++ app/src/uhome.php
@@ -5,19 +5,22 @@
 <html>
 <body>
 
-<div align="right">
-  <h4><a href="logout.php">LogOut</a> </h4>
+<div>
+	<div align="right">
+	  	<h4><a href="logout.php">LogOut</a> </h4>
+	</div>
+	<div align = "left">
+		<form action="/forum.php">
+		  New DISCUSSION : <br>
+		  <input type="text" name="firstname" >
+		  <br>
+		  VIEWS : <br>
+		  <input type="text" name="lastname"  >
+		  <br><br>
+		  <input type="submit" value="Submit">
+		</form> 
+	</div>
 </div>
-
-<form action="/action_page.php">
-  New DISCUSSION : <br>
-  <input type="text" name="firstname" value="Mickey">
-  <br>
-  VIEWS : <br>
-  <input type="text" name="lastname" value="Mouse">
-  <br><br>
-  <input type="submit" value="Submit">
-</form> 
 
 </body>
 </html>
