d9dc5004fc3a990d3e61b97d76472bc806d1eed9
Tarun4444 <tarungulati2013@gmail.com>
2017-09-11 01:12:50 +0530

    2

--- app/src/uhome.php
+++ app/src/uhome.php
@@ -2,13 +2,23 @@
 	require("header.php");
 ?>
 
+<html>
+<body>
+
 <h4><a href="logout.php">LogOut</a> </h4> 
 
+<form action="/action_page.php">
+  New DISCUSSION : <br>
+  <input type="text" name="firstname" value="Mickey">
+  <br>
+  VIEWS : <br>
+  <input type="text" name="lastname" value="Mouse">
+  <br><br>
+  <input type="submit" value="Submit">
+</form> 
 
+</body>
+</html>
 
-<?php
-
-	
-?>
 
 <?php require("footer.php");?>
