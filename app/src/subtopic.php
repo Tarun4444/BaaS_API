465b083e535a8aa83729dab32b362906f692e6cf
Tarun4444 <tarungulati2013@gmail.com>
2017-09-12 17:16:46 +0530

    2

--- app/src/subtopic.php
+++ app/src/subtopic.php
@@ -8,6 +8,47 @@
 	  var discussion_id = '<?= $_GET['d_id']?>' ;
 
 	  function answer_query(discussion_id){ 
+
+	  var request_d= new XMLHttpRequest();	   
+            request_d.onreadystatechange = function () {
+        
+          if (request_d.readyState === XMLHttpRequest.DONE) {
+            if (request_d.status === 200) {
+               
+               //console.log(this.responseText);  // returns Arrray of Objects
+               ordered_d=JSON.parse(this.responseText);
+	       //order_d_id=JSON.parse(this.responseText).id;
+	       var employ = JSON.stringify(orderedanswers);
+              // console.log(employee);
+		
+		var employs = JSON.parse(employ);
+		//console.log(employees);
+
+		var tabl = document.createElement("table");
+		for (var i = 0; i < employs.length; i++) {
+			  var row = tabl.insertRow(-1);
+			  var firstNameCell = row.insertCell(-1);
+			  //var ii = document.createElement("a");
+			//ii.setAttribute("href","subtopic.php?d_id="+employees[i].discussion_id);
+			//ii.appendChild(document.createTextNode(employees[i].discussion_topic));
+			firstNameCell.appendChild(document.createTextNode(employees[i].discussion_topic));
+			var lastNameCell = row.insertCell(-1);
+			  lastNameCell.appendChild(document.createTextNode(employees[i].discussion_detail));
+			}
+			document.body.appendChild(tabl);
+		
+	    }
+	}
+    }  
+   
+            request_d.open('POST', " https://data.bewitch58.hasura-app.io/v1/query ", true);
+	    request_d.setRequestHeader('Content-type','application/json');
+	    request_d.setRequestHeader('Authorization','Bearer '+admin_toke);
+	    request_d.send(JSON.stringify({"type":"select","args":{"table":"answer","columns":["*"],"where":			{"refer_discussion_id":discussion_id}}}));
+
+
+
+
 	  var request= new XMLHttpRequest();	   
             request.onreadystatechange = function () {
         
@@ -50,3 +91,6 @@ answer_query(discussion_id);
 };
 </script>
 
+<?php 
+ require("footer.php");
+?>
