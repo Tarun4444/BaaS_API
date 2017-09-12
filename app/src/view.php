43713920ff266634ed7b831f2db5444ab60902a1
Tarun4444 <tarungulati2013@gmail.com>
2017-09-12 18:23:25 +0530

    2

---
+++ app/src/view.php
@@ -0,0 +1,38 @@
+<script>
+  
+ var u_id = "; "+document.cookie ;
+	//var poop=u_id.search("authio");
+	u_id = u_id.split("; user_id=");
+	if(u_id.length==2)
+    		u_id=u_id.pop().split(";").shift(); 
+	var uu_id = parseInt(u_id) ;
+	console.log(uu_id);
+ 
+ function view_query(){
+ 	
+    var request= new XMLHttpRequest();
+   
+    request.onreadystatechange= function(){
+   
+      if(request.readyState===XMLHttpRequest.DONE){
+        
+         if(request.status===200) 
+         {
+ 	   console.log("discussioned");
+	   window.location.href = "https://myapp.bewitch58.hasura-app.io/subtopic.php?d_id="+uu_id;
+         }
+     }               
+   
+   }         
+   request.open('POST', " https://data.bewitch58.hasura-app.io/v1/query ", true);
+	    request.setRequestHeader('Content-type','application/json');
+	    request.setRequestHeader('Authorization','Bearer '+admin_toke);
+	    request.send(JSON.stringify({"type":"insert","args":{"table":"answer","objects":[{"answer_detail":d_view}]}}));
+
+}
+
+var d_view = '<?= $_POST['d_v']?>' ;
+
+view_query(d_v);
+
+</script> 
