1cc2edf1bd53a91f545c824acca7de7b34ff5fa0
Tarun4444 <tarungulati2013@gmail.com>
2017-09-12 15:45:29 +0530

    2

--- app/src/loginH.php
+++ app/src/loginH.php
@@ -16,7 +16,7 @@
                     	console.log(this.responseText);    // returns JSON object
                     	console.log("user_id = "+user_id + auth_toke);  // user_id = 8 
 			
-			document.cookie = "user_id" + user_id;	
+			document.cookie = "user_id=" + user_id;	
 			document.cookie = "authio="+auth_toke;
 			
 			window.location.href = "https://myapp.bewitch58.hasura-app.io/uhome.php";
